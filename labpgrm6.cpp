27560ef7c213196010a3f78e6859168c2a70612c
bhavya bansal <bhavyabansal12416@gmail.com>
2026-08-15 18:08:36 +0530

    Lab program 6

---
+++ labpgrm6.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+class Demo
+{
+    int val;
+
+public:
+    Demo()
+    {
+        val = 0;
+    }
+    Demo(int v)
+    {
+        val = v;
+        cout << "Parameterized constructor" << endl;
+    }
+    Demo(const Demo &obj)
+    {
+        val = obj.val;
+        cout << "Copy constructor" << endl;
+    }
+
+    void display()
+    {
+        cout<<val<<endl;
+    }
+};
+
+int main()
+{
+ Demo obj1;
+ obj1.display();
+ Demo obj2;
+ obj2.display(50);
+ Demo obj3(obj2);
+ obj3.display();
+    return 0;
+}
