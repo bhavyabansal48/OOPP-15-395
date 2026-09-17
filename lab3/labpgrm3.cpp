8cf370f611aa587bcc206f31c2e6463ae0433035
bhavya bansal <bhavyabansal12416@gmail.com>
2026-09-17 09:31:13 +0530

    Experiment 3

---
+++ lab3/labpgrm3.cpp
@@ -0,0 +1,29 @@
+//implement a program to demostrate inline function , default arguments and function overloading for performing arithmetic operations
+#include<iostream>
+using namespace std;
+
+inline int square(int n){ //inline function
+  return n*n;
+}
+
+int addition(int a ,int b=10){  //default arguments
+  return a+b;
+}
+
+int multiply(int a,int b){  // function overloading
+  return a*b;
+}
+
+double multiply(double a ,double b){
+  return a*b; 
+}
+
+int main(){
+  cout<<"Square of number is : "<<square(5)<<endl;
+  cout<<"Addition of numbers is : "<<addition(1)<<endl;
+  cout<<"Multiplication of numbers is : "<<endl;
+  cout<<multiply(3,7)<<endl;
+  cout<<multiply(3.5,9.0);
+
+  return 0;
+}
